b06dbf12eea6203a882a110de3bbacbcd41ce4a4
mecry makwarela <makwarelamercykgaogelo@gmail.com>
2021-02-17 23:54:19 +0200

    cafeDroidMenu

--- app/src/main/java/com/example/cafedroidInput/MainActivity.java
+++ app/src/main/java/com/example/cafedroidInput/MainActivity.java
@@ -53,11 +53,33 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
+     //   int id = item.getItemId();
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+     //   if (id == R.id.action_order) {
+          //  String stringMessage = (R.string.action_order_message);
+         //   displayToast(stringMessage);
+
+    //        return true;
+      //  }
+        switch (item.getItemId()){
+            case R.id.action_order:
+              //  displayToast(getString(R.string.action_order_message));
+                Intent intent = new Intent(MainActivity.this, orderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+          //  break;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                break;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                break;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                break;
+            default:
         }
 
         return super.onOptionsItemSelected(item);
